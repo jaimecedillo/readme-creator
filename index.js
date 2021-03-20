0b3725adfc77e6404cdb45d8eeb5f778bd6dbbb7
jaime <jaime@MacBook-Pro.local>
2021-03-20 16:08:40 -0600

    created function to init app and added writefile

--- index.js
+++ index.js
@@ -1,77 +1,89 @@
 // TODO: Include packages needed for this application
 const fs = require('fs')
 const inquirer = require('inquirer');
+const util = require('util');
+const writeFileAsync = util.promisify(fs.writeFile);
+const generateMarkdown = require('./src/generateMarkdown')
+
+
 // TODO: Create an array of questions for user input
 const questions = () => {
     inquirer.prompt([
-            {
-                type: 'input',
-                name: 'title',
-                message: 'What is your project title?',
-            },
-            {
-                type: 'input',
-                name: 'description',
-                message: 'Give a detailed description of your project:',
-            },
-            {
-                type: 'input',
-                name: 'installation',
-                message: 'Give installation process for your project:',
-            },
-            {
-                type: 'input',
-                name: 'usage',
-                message: 'What is the project used for?',
-            },
-            {
-                type: 'input',
-                name: 'credits',
-                message: 'List people that contributed to this project.',
-            },
-            {
-                type: 'list',
-                name: 'license',
-                message: 'Chose the license for this project',
-                choices: ["Apache", "Academic", "GNU", "ISC", "MIT", "Mozilla", "Open"]
-            },
-            {
-                type: 'input',
-                name: 'test',
-                message: 'Is there any test for project?',
-            },
-            {
-                type: 'input',
-                name: 'question',
-                message: 'What do I do if I run into an issue?',
-            },
-            {
-                type: 'input',
-                name: 'github',
-                message: 'Enter your GitHub Username (Required)',
-                validate: usernameInput => {
-                    if (usernameInput) {
-                        return true;
-                    } else {
-                        console.log('Please enter your username!');
-                        return false;
-                    }
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is your project title?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Give a detailed description of your project:',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Give installation process for your project:',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What is the project used for?',
+        },
+        {
+            type: 'input',
+            name: 'credits',
+            message: 'List people that contributed to this project.',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Chose the license for this project',
+            choices: ["Apache", "Academic", "GNU", "ISC", "MIT", "Mozilla", "Open"]
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Is there any test for project?',
+        },
+        {
+            type: 'input',
+            name: 'question',
+            message: 'What do I do if I run into an issue?',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub Username (Required)',
+            validate: usernameInput => {
+                if (usernameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your username!');
+                    return false;
                 }
-            },
-            {
-                type: 'input',
-                name: 'email',
-                message: 'Enter your Email:',
-            },
-        ]);
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your Email:',
+        },
+    ]);
 }
-questions()
 
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) { }
+
+
 
 // // TODO: Create a function to initialize app
-// function init() { }
-
+async function init() {
+    try {
+        const data = await questions();
+        const createReadme = generateMarkdown(data);
+        await writeFileAsync('./dist/README.md', createReadme);
+        console.log('Readme created successfully!');
+    } catch (err) {
+        console.log(err)
+    }
+}
 // // Function call to initialize app
-// init();
+init();
